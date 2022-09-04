5e85d4a88aa6d2f97223e07f08b3ec9691da8b46
MykolaPom <mykola.pomazanov@gmail.com>
2022-09-04 13:03:03 +0300

    Task 3 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,48 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
+// const throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+// localStorage.setItem('videoplayer-current-time', 'time');
+// timeupdate;
+console.log(localStorage.getItem('videoplayer-current-time'));
+
+
+const onPlay = function (data) {
+
+  // let currentPlayTime = 0;
+
+
+
+
+
+  // player
+  //   .setCurrentTime(30.456)
+  //   .then(function (seconds) {
+  //     // seconds = the actual time that the player seeked to
+  //   })
+  //   .catch(function (error) {
+  //     switch (error.name) {
+  //       case 'RangeError':
+  //         // the time was less than 0 or greater than the video’s duration
+  //         break;
+
+  //       default:
+  //         // some other error occurred
+  //         break;
+  //     }
+  //   });
+
+// console.log('played the video!');
+
+  // data is an object containing properties specific to that event
+};
+
+// player.on(throttle(1000), onPlay);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,56 @@
+import { throttle } from 'lodash';
 
+
+//збереження значення полів у локальне сховище
+const form = document.querySelector('.feedback-form');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify({ email: '', message: '' }));
+
+form.addEventListener('keydown', throttle(saveForm, 500));
+
+const { email, message } = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+form.email.value = email;
+form.message.value = message;
+
+form.addEventListener('submit', submitForm);
+
+function saveForm() {
+  localStorage.setItem(
+    LOCALSTORAGE_KEY,
+    JSON.stringify({ email: form.email.value, message: form.message.value })
+    );
+}
+  
+function submitForm(e) {
+  e.preventDefault();
+  console.log({ email: form.email.value, message: form.message.value });
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+  form.reset();
+}
+
+  
+  
+
+
+// const refs = {
+//   form: document.querySelector('.feedback-form'),
+//   input: document.querySelector('.feedback-form input'),
+//   textarea: document.querySelector('.feedback-form textarea'),
+// };
+
+// console.log(refs);
+
+// refs.form.addEventListener('submit', onFormSubmit);
+// refs.form.addEventListener('input', onTextareaInput);
+
+// function onFormSubmit(event) {
+
+
+// }
+
+// function onTextareaInput(event) {
+//   const value = event.currentTarget.value;
+//   console.log(value);
+// }
