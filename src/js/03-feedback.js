9e8acf845d1d901b6a44cb50dff1e8d790b942f9
MykolaPom <mykola.pomazanov@gmail.com>
2022-09-04 19:46:52 +0300

    Tast 3 changes

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,31 +1,48 @@
-import { throttle } from 'lodash';
-
+import throttle from 'lodash.throttle';
 
 //збереження значення полів у локальне сховище
-const form = document.querySelector('.feedback-form');
+const formEl = document.querySelector('.feedback-form');
+const inputEl = document.querySelector('.feedback-form input');
+const textareaEl = document.querySelector('.feedback-form textarea');
 const LOCALSTORAGE_KEY = 'feedback-form-state';
 
-localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify({ email: '', message: '' }));
+const formData = {};
 
-form.addEventListener('keydown', throttle(saveForm, 500));
 
-const { email, message } = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+formEl.addEventListener('input', throttle(saveForm, 500));
+formEl.addEventListener('submit', submitForm);
 
-form.email.value = email;
-form.message.value = message;
-
-form.addEventListener('submit', submitForm);
-
-function saveForm() {
-  localStorage.setItem(
-    LOCALSTORAGE_KEY,
-    JSON.stringify({ email: form.email.value, message: form.message.value })
-    );
-}
+function saveForm(e) {
+  formData.email = inputEl.value;
   
+  formData.message = textareaEl.value;
+  
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+
 function submitForm(e) {
   e.preventDefault();
-  console.log({ email: form.email.value, message: form.message.value });
+ 
+  formData.email = inputEl.value;
+  formData.message = textareaEl.value;
+  
+  console.log(formData);
+
   localStorage.removeItem(LOCALSTORAGE_KEY);
-  form.reset();
+  formEl.reset();
 }
+
+function populateTextarea() {
+  if (localStorage.getItem(LOCALSTORAGE_KEY)) {
+    const savedData = localStorage.getItem(LOCALSTORAGE_KEY);
+    const parsedData = JSON.parse(savedData);
+
+    textareaEl.value = parsedData.message;
+    inputEl.value = parsedData.email;
+    
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(parsedData));
+  };
+};
+
+populateTextarea();
+
