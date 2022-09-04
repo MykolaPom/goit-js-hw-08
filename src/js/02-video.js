304bbfc16529d60bd3c7a247809a8b831952b01a
MykolaPom <mykola.pomazanov@gmail.com>
2022-09-04 20:21:17 +0300

    Task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,48 +1,18 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-// const throttle = require('lodash.throttle');
+const PLAYER_STORAGE_KEY = 'videoplayer-current-time';
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Vimeo.Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+player.on(
+  'timeupdate',
+  throttle(function () {
+    player.getCurrentTime().then(function (seconds) {
+      localStorage.setItem(PLAYER_STORAGE_KEY, seconds);
+    });
+  }, 1000)
+);
 
-// localStorage.setItem('videoplayer-current-time', 'time');
-// timeupdate;
-console.log(localStorage.getItem('videoplayer-current-time'));
-
-
-const onPlay = function (data) {
-
-  // let currentPlayTime = 0;
-
-
-
-
-
-  // player
-  //   .setCurrentTime(30.456)
-  //   .then(function (seconds) {
-  //     // seconds = the actual time that the player seeked to
-  //   })
-  //   .catch(function (error) {
-  //     switch (error.name) {
-  //       case 'RangeError':
-  //         // the time was less than 0 or greater than the video’s duration
-  //         break;
-
-  //       default:
-  //         // some other error occurred
-  //         break;
-  //     }
-  //   });
-
-// console.log('played the video!');
-
-  // data is an object containing properties specific to that event
-};
-
-// player.on(throttle(1000), onPlay);
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
